8a2f960860e211310deb7b22ca8c35f65f49129a
Dave Stewart <dave@davestewart.co.uk>
2016-03-18 17:49:28 +0000

    Added control() helper to CrudServiceProvider

--- src/CrudServiceProvider.php
+++ src/CrudServiceProvider.php
@@ -1,37 +1,55 @@
-<?php namespace davestewart\laravel\crud;
+<?php
 
-use davestewart\laravel\crud\CrudRepository;
-use Illuminate\Support\ServiceProvider;
-
-/**
- * Class CrudServiceProvider
- *
- * Provides default functionality for Eloquent models
- *
- * @package davestewart\laravel\crud
- */
-class CrudServiceProvider extends ServiceProvider {
+namespace davestewart\laravel\crud
+{
+	use Illuminate\Support\ServiceProvider;
 
 	/**
-	 * Bootstrap the application services.
+	 * Class CrudServiceProvider
 	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		//
-	}
-
-	/**
-	 * Register the application services.
+	 * Provides default functionality for Eloquent models
 	 *
-	 * @return void
+	 * @package davestewart\laravel\crud
 	 */
-	public function register()
-	{
-		$this->app->bind('CrudService', 'davestewart\laravel\crud\CrudService');
-		$this->app->bind('CrudField',   'davestewart\laravel\crud\CrudField');
-		$this->app->bind('CrudRepo',    'davestewart\laravel\crud\repos\EloquentRepo');
-	}
+	class CrudServiceProvider extends ServiceProvider {
 
+		/**
+		 * Bootstrap the application services.
+		 *
+		 * @return void
+		 */
+		public function boot()
+		{
+			//
+		}
+
+		/**
+		 * Register the application services.
+		 *
+		 * @return void
+		 */
+		public function register()
+		{
+			$this->app->bind('CrudService', 'davestewart\laravel\crud\CrudService');
+			$this->app->bind('CrudRepo',    'davestewart\laravel\crud\repos\EloquentRepo');
+			$this->app->bind('CrudField',   'davestewart\laravel\crud\CrudField');
+			$this->app->bind('CrudControl', 'davestewart\laravel\crud\controls\FormControl');
+		}
+
+	}
+}
+
+namespace
+{
+
+	use davestewart\laravel\crud\CrudField;
+
+	if( ! function_exists('control') )
+	{
+		function control(CrudField $field)
+		{
+			return \App::make('CrudControl', [$field]);
+
+		}
+	}
 }
