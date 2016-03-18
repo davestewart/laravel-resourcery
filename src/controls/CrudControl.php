6c79b15e2507d4c8d396101b347ace6a85721418
Dave Stewart <dave@davestewart.co.uk>
2016-03-18 20:44:38 +0000

    Added CrudControl and FormControl classes

---
+++ src/controls/CrudControl.php
@@ -0,0 +1,278 @@
+<?php namespace davestewart\laravel\crud\controls;
+
+use davestewart\laravel\crud\CrudField;
+use davestewart\laravel\crud\errors\InvalidPropertyException;
+use Illuminate\Support\MessageBag;
+use Input;
+use Session;
+
+class CrudControl
+{
+
+	// ------------------------------------------------------------------------------------------------
+	// properties
+
+		/** @var CrudField|mixed  */
+		protected $field;
+
+		/** @var array  */
+		protected $classes;
+
+		/** @var array  */
+		protected $attributes;
+
+
+	// ------------------------------------------------------------------------------------------------
+	// instantiation
+
+		public static function __callStatic($name, $params)
+		{
+			return call_user_func_array([get_called_class(), 'create'], array_merge([$name], $params));
+		}
+
+	/**
+	 * Creates a control without
+	 *
+	 * @param   string          $type
+	 * @param   string          $name
+	 * @param   array|null      $options
+	 * @param   array           $input
+	 * @param   MessageBag|null $errors
+	 * @return  self
+	 */
+		public static function create($type, $name, array $options = null, array $input = null, MessageBag $errors = null)
+		{
+			// variables
+			$control        = new static;
+			$field          = $control->field;
+			$errors         = isset($errors) ? $errors : Session::get('errors', new MessageBag);
+			$input          = isset($input) ? $input : Input::all();
+
+			// populate field
+			$field->type    = $type;
+			$field->name    = $name;
+			$field->label   = ucwords($name);
+			$field->value   = is_object($input) ? $input->$name : $input[$name];
+			$field->error   = $errors ? $errors->first($name) : null;
+			$field->options = $options;
+			$field->view    = 'vendor.crud.partials.field';
+
+			// debug
+			//pd($field);
+
+			// return
+			return $control;
+		}
+
+		public function __construct(CrudField $field = null)
+		{
+			// properties
+			$this->field        = $field ? $field : \App::make('CrudField');
+			$this->attributes   = [];
+			$this->classes      = [];
+
+			// initialize
+			$this->initialize();
+		}
+
+		protected function initialize()
+		{
+			$this
+				->setAttr('id', $this->field->name)
+				->setAttr('class', 'form-control')
+				->setAttr('autocomplete', 'off');
+
+			if(preg_match('/\brequired\b/', $this->field->rules))
+			{
+				$this->setClass('required');
+			}
+		}
+
+
+	// ------------------------------------------------------------------------------------------------
+	// public methods
+
+		public function set($name, $value)
+		{
+			if(property_exists($this->field, $name))
+			{
+				$this->field->$name = $value;
+			}
+			return $this;
+		}
+
+		public function setClass($name = null, $state = 1)
+		{
+			if ($name)
+			{
+				if ($state)
+				{
+					$this->classes[$name] = 1;
+				}
+				else
+				{
+					unset($this->classes[$name]);
+				}
+			}
+			else
+			{
+				$this->classes = [];
+			}
+			return $this;
+		}
+
+		public function setAttr($name, $value = null)
+		{
+			if($value == null)
+			{
+				unset($this->attributes[$name]);
+			}
+			else if(is_array($name))
+			{
+				foreach($name as $key => $value)
+				{
+					$this->setAttr($key, $value);
+				}
+			}
+			else
+			{
+				if($name == 'class')
+				{
+					$this->addAttr($name, $value);
+				}
+				else
+				{
+					$this->attributes[$name] = $value;
+				}
+			}
+			return $this;
+		}
+
+		public function addAttr($name, $value)
+		{
+			if( ! isset($this->attributes[$name]) )
+			{
+				$this->attributes[$name] = $value;
+			}
+			else
+			{
+				$this->attributes[$name] .= ' ' . $value;
+			}
+			return $this;
+		}
+
+		/**
+		 * Magic getter
+		 *
+		 * Get field or control properties
+		 *
+		 * @param $name
+		 * @return mixed|string
+		 * @throws InvalidPropertyException
+		 */
+		public function __get($name)
+		{
+			if(property_exists($this, $name))
+			{
+				return $this->$name;
+			}
+			else if(property_exists($this->field, $name))
+			{
+				return $this->field->$name;
+			}
+			else if(isset($this->attributes[$name]))
+			{
+				return $this->attributes[$name];
+			}
+			else if($name == 'element')
+			{
+				return $this->render(true);
+			}
+			throw new InvalidPropertyException($name, get_called_class());
+		}
+
+		public function __call($name, $params)
+		{
+			$value = $params[0];
+			switch($name)
+			{
+				case 'class':
+					$this->setClass($name, $value);
+					break;
+
+				case 'attr':
+					$this->setAttr($params[0], $params[1]);
+					break;
+
+				default:
+					if(is_object($value))
+					{
+						$this->field->value = $value->{$this->field->name};
+					}
+					else if(property_exists($this->field, $name))
+					{
+						$this->field->$name = $value;
+					}
+					else
+					{
+						$this->setAttr($name, $value);
+					}
+			}
+			return $this;
+		}
+
+
+	// ------------------------------------------------------------------------------------------------
+	// render methods
+
+		public function render($view = null)
+		{
+			// values
+			$field      = $this->field;
+			$attributes = $this->getControlAttrs();
+			$classes    = implode(' ', $this->getGroupClasses($field));
+
+			// control
+			$method     = "make_{$field->type}";
+			$method     = method_exists($this, $method) ? $method : 'make_text';
+			$control    = $this->$method($field, $attributes);
+			if($view === true)
+			{
+				return $control;
+			}
+
+			// view
+			$view       = is_string($view)
+							? $view
+							: $field->view;
+
+			// return
+			return view($view, compact('field', 'control', 'classes'));
+		}
+
+		public function debug()
+		{
+			return '<pre>' . print_r($this, 1) . '</pre>';
+		}
+
+
+	// ------------------------------------------------------------------------------------------------
+	// utilities
+
+		protected function getControlAttrs($attrs = [])
+		{
+			return $this->attributes + $attrs;
+		}
+
+		protected function getGroupClasses($field)
+		{
+			$classes    = $field->error ? ['has-error'] : [];
+			$classes    += array_keys($this->classes);
+			return $classes;
+		}
+
+		public function __toString()
+		{
+			return (string) $this->render();
+		}
+}
---
+++ src/controls/FormControl.php
@@ -0,0 +1,87 @@
+<?php namespace davestewart\laravel\crud\controls;
+
+use davestewart\laravel\crud\CrudField;
+use Form;
+
+class FormControl extends CrudControl
+{
+
+	protected function make_text(CrudField $field, array $attrs = [])
+	{
+		return Form::input($field->type, $field->name, $field->value, $attrs);
+	}
+
+	protected function make_textarea(CrudField $field, array $attrs = [])
+	{
+		return Form::textarea($field->name, $field->value, $attrs);
+	}
+
+	protected function make_info(CrudField $field, array $attrs = [])
+	{
+		return Form::text($field->name, $field->value, array_merge($attrs, ['readonly' => 'readonly']));
+	}
+
+	protected function make_select(CrudField $field, array $attrs = [])
+	{
+		return Form::select($field->name, $field->options, $field->value, $attrs);
+	}
+
+	protected function make_radios(CrudField $field, array $attrs = [])
+	{
+		return $this->make_control_group($field, $attrs);
+	}
+
+	protected function make_checkboxes(CrudField $field, array $attrs = [])
+	{
+		if( ! is_array($field->value) )
+		{
+			throw new \Exception('make_checkboxes() expects the passed field->value to be an array');
+		}
+		return $this->make_control_group($field, $attrs);
+	}
+
+	protected function make_checkbox(CrudField $field, array $attrs = [])
+	{
+		Form::checkbox($field->name, $field->value, !! $field->value);
+	}
+
+	protected function make_control_group(CrudField $field, array $attrs = [])
+	{
+		// variables
+		$index  = 0;
+		$html   = '<div class="controls-group">';
+
+		// loop
+		foreach($field->options as $value => $label)
+		{
+			// common values
+			$id     = $field->name . '_' . $value;
+			$name   = $field->name;
+
+			// control-specific values
+			if($field->type == 'radios')
+			{
+				$method     = 'radio';
+				$checked    = $field->value == null && $index++ == 0 // always check e
+								? true
+								: $field->value == $value;
+			}
+			else
+			{
+				$name       .= '[]';
+				$method     = 'checkbox';
+				$checked    = in_array($value, $field->value);
+			}
+
+			// html
+			$html .= '<div class="control-group">';
+			$html .= Form::$method($name, $value, $checked, ['id' => $id]);
+			$html .= ' ';
+			$html .= Form::label($id, $label);
+			$html .= '</div>';
+		}
+		$html .= '</div>';
+		return $html;
+	}
+
+}
