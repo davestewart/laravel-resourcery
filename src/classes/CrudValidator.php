67bd564377b9b31f799c12b2698a61df62d759db
Dave Stewart <dave@davestewart.co.uk>
2016-04-03 01:20:30 +0100

    Update CrudValidator ready to be used with new factory

--- src/classes/CrudValidator.php
+++ src/classes/CrudValidator.php
@@ -3,24 +3,16 @@
 use Illuminate\Translation\Translator;
 use Illuminate\Validation\Validator;
 
+/**
+ * Validation class with custom messages
+ *
+ * Also fixes a bug where array messages for size attributes don't return the correct element
+ */
 class CrudValidator extends Validator
 {
-	public function __construct(array $data, array $rules)
+	public function __construct(Translator $translator, array $data, array $rules)
 	{
-		/**  @var Translator $trans */
-		$trans      = \App::make('translator');
-
-		/** @var array $messages */
-		$messages   = trans('resourcery::validation');
-
-		// parent
-		parent::__construct($trans, $data, $rules, $messages);
-
-		// set up machinery for unique validation
-		$app = app();
-		if (isset($app['validation.presence'])) {
-			$this->setPresenceVerifier($app['validation.presence']);
-		}
+		parent::__construct($translator, $data, $rules, trans('resourcery::validation'));
 	}
 
 	protected function getInlineMessage($attribute, $lowerRule, $source = null)
