29d37af9328fb9c76e3fc84c13813116e98abcc5
Dave Stewart <dave@davestewart.co.uk>
2016-04-06 01:03:51 +0100

    Updated comments

--- src/classes/exceptions/InvalidPropertyException.php
+++ src/classes/exceptions/InvalidPropertyException.php
@@ -1,10 +1,22 @@
 <?php namespace davestewart\resourcery\classes\exceptions;
 
+use Exception;
 
-class InvalidPropertyException extends \Exception
+/**
+ * Class InvalidPropertyException
+ *
+ * Typically thrown when an attempt is made to access a non-existent property by way of __get()
+ */
+class InvalidPropertyException extends Exception
 {
+	/**
+	 * InvalidPropertyException constructor.
+	 *
+	 * @param string $name
+	 * @param int    $class
+	 */
 	public function __construct($name, $class)
 	{
-		$this->message = "Property `$name` does not exist on object `$class`";
+		$this->message = "Property '$name' does not exist on object '$class'";
 	}
 }
